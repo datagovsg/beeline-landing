86f5dfc9801fa8cc775d49f95c50eb529f156351
daniel <daniel_sim@data.gov.sg>
2016-12-22 16:29:49 +0800

    fix /new on start

--- static/src/components/crowdstart-from-route.vue
+++ static/src/components/crowdstart-from-route.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <ol v-if="crowdstarted.arrivalTime">
-      <li v-for="stop in computedStops">
-        <b>{{stop.time | formatTime}}</b>
+    <template v-if="!route || !route.trips || !route.trips[0]">
+      Please <router-link to="/search">search</router-link> for a route first.
+    </template>
+    <template v-else>
+      <ol v-if="arrivalTime">
+        <li v-for="stop in computedStops">
+          <b>{{stop.time | formatTime}}</b>
 
-        {{stop.stop.description}}
-      </li>
-    </ol>
-    <ol v-else>
-      <li v-for="stop in computedStops">
-        <b>??:??</b>
+          {{stop.stop.description}}
+        </li>
+      </ol>
+      <ol v-else>
+        <li v-for="stop in computedStops">
+          <b>??:??</b>
 
-        {{stop.stop.description}}
-      </li>
-    </ol>
+          {{stop.stop.description}}
+        </li>
+      </ol>
 
-    <label>
-      Arrive at
-      <select v-model="crowdstarted.arrivalTime">
-        <option value=""></option>
-        <option value="07:45">07:45</option>
-        <option value="08:00">08:00</option>
-        <option value="08:15">08:15</option>
-        <option value="08:30">08:30</option>
-        <option value="08:45">08:45</option>
-        <option value="09:00">09:00</option>
-      </select>
-    </label>
+      <label>
+        Arrive at
+        <select v-model="arrivalTime">
+          <option value=""></option>
+          <option value="07:45">07:45</option>
+          <option value="08:00">08:00</option>
+          <option value="08:15">08:15</option>
+          <option value="08:30">08:30</option>
+          <option value="08:45">08:45</option>
+          <option value="09:00">09:00</option>
+        </select>
+      </label>
+    </template>
   </div>
 </template>
 
@@ -39,16 +44,14 @@ export default {
   props: ['route'],
   data() {
     return {
-      crowdstarted: {
-        arrivalTime: '',
-      }
+      arrivalTime: '',
     }
   },
   computed: {
     crowdstartUrl() {
       return `https://app.beeline.sg/#/tabs/crowdstart/${this.route.id}/detail`
     },
-    arrivalTime() {
+    originalArrivalTime() {
       var tripStops = _.sortBy(this.route.trips[0].tripStops, ts => ts.time);
       return moment(tripStops[tripStops.length-1].time).utcOffset(480).format('hh:mm');
     },
@@ -56,7 +59,7 @@ export default {
       var currentStops = _.sortBy(this.route.trips[0].tripStops, 'time');
       var currentTimes = currentStops.map(s => moment(s.time).utcOffset(480))
       var today = moment();
-      var [hours, minutes] = this.crowdstarted.arrivalTime ? this.crowdstarted.arrivalTime.split(':') : [0, 0];
+      var [hours, minutes] = this.arrivalTime ? this.arrivalTime.split(':') : [0, 0];
 
       var newTimes = currentTimes.map(ct => {
         var timeDifference = currentTimes[currentTimes.length - 1].valueOf() - ct.valueOf()
