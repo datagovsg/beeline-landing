6cc88c42ddce784ed5b3e5c4814aa011d073c236
Melvin <melvin0719@hotmail.com>
2017-05-19 10:39:32 +0800

    Make google maps bounds code more elegant

--- static/src/components/crowdstart-viewer.vue
+++ static/src/components/crowdstart-viewer.vue
@@ -11,7 +11,8 @@
         </ol>
       </div>
 
-      <gmap-map class="gmap" :center="{lat: centerLat, lng: centerLng}" :zoom="12">
+      <gmap-map class="gmap" :center="{lat: 1.38, lng: 103.8}" :zoom="12"
+        ref="crowdstart-route-map">
         <gmap-marker v-for="stop in crowdstart.userSuggestedRouteStops"
           :position="{lat: stop.stop.coordinates.coordinates[1], lng: stop.stop.coordinates.coordinates[0]}">
         </gmap-marker>
@@ -23,34 +24,39 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   props: ['crowdstart', 'index'],
   filters: require('../utils/filters').default,
   computed: {
-    centerLat() {
-      var numStops = this.crowdstart.userSuggestedRouteStops.length
-      if (numStops === 0) {
-        return 1.38;
-      }
-      var firstStop = this.crowdstart.userSuggestedRouteStops[0]
-      var lastStop = this.crowdstart.userSuggestedRouteStops[numStops - 1]
-      return (firstStop.stop.coordinates.coordinates[1]
-        + lastStop.stop.coordinates.coordinates[1]) / 2;
-    },
-    centerLng() {
-      var numStops = this.crowdstart.userSuggestedRouteStops.length
-      if (numStops === 0) {
-        return 103.8;
-      }
-      var firstStop = this.crowdstart.userSuggestedRouteStops[0]
-      var lastStop = this.crowdstart.userSuggestedRouteStops[numStops - 1]
-      return (firstStop.stop.coordinates.coordinates[0]
-        + lastStop.stop.coordinates.coordinates[0]) / 2;
-    },
     pathCoordinates() {
-      return (this.crowdstart.path) ? this.crowdstart.path.coordinates.map(c => ({lat: c[0], lng: c[1]})) : []
+      return (this.crowdstart.path) ? 
+        this.crowdstart.path.coordinates.map(c => ({lat: c[0], lng: c[1]})) : []
+    },
+    stopsCoordinates() {
+      return (this.crowdstart.userSuggestedRouteStops) ?
+        this.crowdstart.userSuggestedRouteStops
+          .map(stop => stop.stop.coordinates.coordinates)
+          .map(c => ({lat: c[1], lng: c[0]})) 
+        : []
     }
   },
+  mounted() {
+    this.$refs['crowdstart-route-map'].$deferredReadyPromise.then(() => {
+      this.refit();
+    })
+  },
+  methods: {
+    refit() {
+      var bounds = new google.maps.LatLngBounds();
+      for (let c of this.stopsCoordinates) {
+        bounds.extend(c)
+      }
+      this.$refs['crowdstart-route-map'].resize();
+      this.$refs['crowdstart-route-map'].$mapObject.fitBounds(bounds);
+    },
+  }
 }
 </script>
 
