e082e20aa6640627ed7621795978a34b169cd587
Daniel Sim <daniel_sim@data.gov.sg>
2017-09-05 16:30:36 +0800

    privacy policy and terms of use

--- components/landing/Footer.vue
+++ components/landing/Footer.vue
@@ -6,9 +6,9 @@
       <p class="pull-left">&copy; 2016 Beeline Singapore. &nbsp; All rights reserved.</p>
       <p class="pull-right">
         <a class="links" href="#top">Back to top</a>&middot;
-        <nuxt-link class="links" to="/privacy_policy.html" target="_blank">Privacy Policy</nuxt-link>
+        <nuxt-link class="links" to="/privacy_policy.html">Privacy Policy</nuxt-link>
         &middot;
-        <nuxt-link class="links" to="/terms_of_use.html" target="_blank">Terms of Use</nuxt-link>
+        <nuxt-link class="links" to="/terms_of_use.html">Terms of Use</nuxt-link>
       </p>
     </div>
   </footer>
