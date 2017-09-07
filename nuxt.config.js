f0bd2a64a9df07f36195f27a060b5d9c0cb69ee2
daniel <daniel_sim@data.gov.sg>
2017-09-07 16:55:07 +0800

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Beeline.sg - Singapore\'s marketplace for crowdsourced bus services' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.ico' }
     ]
   },
   /*
