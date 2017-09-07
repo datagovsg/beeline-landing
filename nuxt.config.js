371660d33d695ffc08aeb82af5a8655d277fc0c8
daniel <daniel_sim@data.gov.sg>
2017-09-07 16:55:50 +0800

    tweak the general description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Beeline.sg - Singapore\'s marketplace for crowdsourced bus services' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
