05c04ffe06de7549d0f24f81a233f0aa085f2483
Daniel Sim <daniel_sim@data.gov.sg>
2017-10-05 10:35:15 +0800

    Aesthetic improvements to the autogenerated routes page

---
+++ components/ExpandingTriangle.vue
@@ -0,0 +1,22 @@
+<template>
+<span class="mdi mdi-triangle expanding-triangle"
+  :class="{active}"></span>
+</template>
+<script>
+export default {
+  props: ['active']
+}
+</script>
+<style>
+.expanding-triangle {
+  transition: transform 0.3s;
+  transform-origin: center;
+  display: inline-block;
+}
+.expanding-triangle.active {
+  transform: rotate(180deg);
+}
+.expanding-triangle:not(.active) {
+  transform: rotate(90deg);
+}
+</style>
---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<template>
+<img src="/images/loading.svg" class="loading-spinner"/>
+</template>
+<style>
+
+  .loading-spinner {
+    width: 200px;
+    height: 200px;
+
+    display: block;
+  }
+</style>
