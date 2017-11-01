3a12c1aa35a7b5b02eb8fa472f3b9fbcdde231e1
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2017-11-01 13:48:56 +0800

    missing sharelink component

---
+++ components/autogenerated/ShareLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="input-group">
+    <input type="text" :value="link" ref="textbox" class="form-control" />
+    <span class="input-group-btn">
+      <button @click="copyToClipboard" class="btn btn-primary">
+        <i class="glyphicon glyphicon-copy"></i>
+      </button>
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['link'],
+
+  methods: {
+    copyToClipboard () {
+      this.$refs.textbox.select()
+      document.execCommand('copy')
+    },
+  }
+}
+</script>
