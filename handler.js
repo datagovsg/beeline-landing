284619e281eedcc03a38d93394f259a9cdd81736
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-24 17:19:42 +0800

    Transpile nuxt locally, boost lambda RAM to 256M

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ require('./util/node-8-backcompat')
 /*
  * Handler definition for https://serverless.com/
  */
-const { Nuxt } = require('nuxt')
+const { Nuxt } = require('./nuxt-es5')
 
 let nuxtConfig = require('./nuxt.config.js')
 nuxtConfig.dev = false
